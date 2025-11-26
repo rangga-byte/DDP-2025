d3206abfa2940903d2c919ba95d5e70eca2475dd
rangga ibnu hafidz <ibnuhafidzrangga@gmail.com>
2025-11-26 11:24:30 +0700

    Add files via upload

---
+++ modul/panggil.py
@@ -0,0 +1,15 @@
+import bangunRuang as br
+
+print("~~~~ BANGUN RUANG ~~~~")
+print(f'volume kubus dengan sisi 3 adalah {br.kubus(3)}')
+print(f'volume balok adalah {br.balok(4, 5, 2)}')
+print(f"volume prisma segitiga adalah {br.prisma(5, 4, 5)}")
+print(f"volume tabung adalah")
+print(f"volume kerucut adalah")
+
+print(" BANGUN DATAR ~~")
+print(f"Luas Persegi adalah ")
+print(f"Luas Persegi Panjang adalah ")
+print(f"Luas Segitiga adalah ")
+print(f"Luas Lingkaran adalah ")
+print(f"Luas JajarGenjang adalah")
